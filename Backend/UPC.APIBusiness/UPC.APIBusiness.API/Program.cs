3267f2e1cfe225e804268946d0ff6632b4a1091e
giancarloobregon <giancarloobregon@gmail.com>
2022-07-11 18:58:22 -0500

    back 01

--- Backend/UPC.APIBusiness/UPC.APIBusiness.API/Program.cs
+++ Backend/UPC.APIBusiness/UPC.APIBusiness.API/Program.cs
@@ -10,15 +10,25 @@ using System.Threading.Tasks;
 
 namespace API
 {
+    // CS1591.cs  
+    // compile with: /W:4 /doc:x.xml  
+
+    /// text  
+#pragma warning disable CS1591 // Falta el comentario XML para el tipo o miembro visible públicamente
     public class Program
+#pragma warning restore CS1591 // Falta el comentario XML para el tipo o miembro visible públicamente
     {
+#pragma warning disable CS1591 // Falta el comentario XML para el tipo o miembro visible públicamente
         public static void Main(string[] args)
+#pragma warning restore CS1591 // Falta el comentario XML para el tipo o miembro visible públicamente
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
+#pragma warning disable CS1591 // Falta el comentario XML para el tipo o miembro visible públicamente
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+#pragma warning restore CS1591 // Falta el comentario XML para el tipo o miembro visible públicamente
     }
 }
